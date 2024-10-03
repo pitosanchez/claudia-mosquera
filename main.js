13d9960c53b5f17f4a09516223acea877f35264f
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-10-03 12:45:16 -0400

    adding gsap to project

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 import './style.css'
 
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.to('.box', {
+    scrollTrigger: '.box', // start animation when ".box" enters the viewport
+    x: 500
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
